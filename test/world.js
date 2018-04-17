f51d758d369fc530176756e7abc1e44fbc98adf6
David Thompson <mail@fatbusinessman.com>
2018-04-17 17:29:21 +0100

    Group and rename world tests for clarity

--- test/world.js
+++ test/world.js
@@ -2,38 +2,44 @@ const assert = require('assert');
 const World = require('../world');
 
 describe('World', () => {
-  it('has a single-cell row', function() {
-    let world = new World(1, 1);
-    assert.deepEqual(world.rows, [[null]]);
+  describe('constructor', function() {
+    it('creates a single-cell world', function() {
+      let world = new World(1, 1);
+      assert.deepEqual(world.rows, [[null]]);
+    });
+
+    it('creates a single-row world', function() {
+      let world = new World(3, 1);
+      assert.deepEqual(world.rows, [[null, null, null]]);
+    });
+
+    it('creates a multi-row world', function() {
+      let world = new World(2, 2);
+      assert.deepEqual(world.rows, [[null, null], [null, null]]);
+    });
   });
 
-  it('has a single-row world', function() {
-    let world = new World(3, 1);
-    assert.deepEqual(world.rows, [[null, null, null]]);
+  describe('withCharacterAt', function() {
+    it('adds a character', function() {
+      let world = new World(1, 1);
+      let character = {};
+      world = world.withCharacterAt(character, 0, 0);
+      assert.deepEqual(world.rows, [[character]]);
+    });
+
+    it('adds a character to a multi-cell world', function() {
+      let world = new World(2, 2);
+      let character = {};
+      world = world.withCharacterAt(character, 0, 0);
+      assert.deepEqual(world.rows, [[character, null], [null, null]]);
+    });
   });
 
-  it('has a multi-row world', function() {
-    let world = new World(2, 2);
-    assert.deepEqual(world.rows, [[null, null], [null, null]]);
-  });
-
-  it('adds a character', function() {
-    let world = new World(1, 1);
-    let character = {};
-    world = world.withCharacterAt(character, 0, 0);
-    assert.deepEqual(world.rows, [[character]]);
-  });
-
-  it('adds a character to a multi-cell world', function() {
-    let world = new World(2, 2);
-    let character = {};
-    world = world.withCharacterAt(character, 0, 0);
-    assert.deepEqual(world.rows, [[character, null], [null, null]]);
-  });
-
-  it('populates the world', function() {
-    let populator = { next: function() { return {c: 1}; } };
-    world = World.populatedBy(2, 2, populator);
-    assert.deepEqual(world.rows, [[{c: 1},{c: 1}],[{c: 1}, {c: 1}]]);
+  describe('populatedBy', function() {
+    it('populates the world', function() {
+      let populator = { next: function() { return {c: 1}; } };
+      world = World.populatedBy(2, 2, populator);
+      assert.deepEqual(world.rows, [[{c: 1},{c: 1}],[{c: 1}, {c: 1}]]);
+    });
   });
 });
