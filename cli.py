33759770b6c79342f1238f70cc4502062cc4b8c5
David Thompson <mail@fatbusinessman.com>
2018-05-14 22:10:13 +0100

    Configure the world parameters from env vars
    This includes a special WORLD_SIZE value of `auto`, which will fit the
    world to the current screen size. This is quite useful for testing,
    especially in a small window in the corner of the screen.

--- cli.py
+++ cli.py
@@ -1,16 +1,36 @@
+from os import environ
+import re
+import shutil
 from time import sleep
 
 from character import Population
 from renderer import Renderer
 from world import World
 
-WORLD_WIDTH = 60
-WORLD_HEIGHT = 30
-HUMAN_DENSITY = 0.05
-ZOMBIE_CHANCE = 0.9
+def _get_world_size(size_string, default):
+    if not size_string:
+        return default
+
+    if size_string == 'auto':
+        terminal_size = shutil.get_terminal_size()
+        return (terminal_size.columns // 2, terminal_size.lines - 1)
+
+    size_match = re.match('(\d+)x(\d+)$', size_string)
+
+    if size_match:
+        return tuple([int(d) for d in size_match.groups()])
+
+    raise ValueError('Unrecognised format "{}"'.format(size_string))
+
+
+world_width, world_height = _get_world_size(environ.get('WORLD_SIZE'),
+                                            default=(60, 30))
+
+HUMAN_DENSITY = float(environ.get('DENSITY', 0.05))
+ZOMBIE_CHANCE = float(environ.get('ZOMBIE_CHANCE', 0.9))
 
 population = Population(HUMAN_DENSITY, ZOMBIE_CHANCE)
-world = World.populated_by(WORLD_WIDTH, WORLD_HEIGHT, population)
+world = World.populated_by(world_width, world_height, population)
 renderer = Renderer(world)
 
 def clear():
