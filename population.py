ad38637bb62142bfbfc98d2d9175dbba33c87add
David Thompson <mail@fatbusinessman.com>
2020-07-20 16:47:20 -0700

    Add type hints to the population module

--- population.py
+++ population.py
@@ -1,8 +1,16 @@
 import random
+from typing import Callable, Generic, Iterable, Iterator, Optional, Tuple, TypeVar
+
+CharacterType = TypeVar("CharacterType")
+Factory = Callable[[], Optional[CharacterType]]
 
 
-class Population:
-    def __init__(self, *probabilities, random_source=random.random):
+class Population(Generic[CharacterType]):
+    def __init__(
+        self,
+        *probabilities: Tuple[float, Factory[CharacterType]],
+        random_source: Callable[[], float] = random.random
+    ):
         self._random = random_source
 
         self._cumulative_probabilities = []
@@ -11,16 +19,16 @@ class Population:
             cumulative_prob += prob
             self._cumulative_probabilities.append((cumulative_prob, factory))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Optional[CharacterType]]:
         return self
 
-    def _factory_for(self, random_value):
+    def _factory_for(self, random_value: float) -> Factory[CharacterType]:
         for (prob, factory) in self._cumulative_probabilities:
             if prob > random_value:
                 return factory
         else:
             return lambda: None
 
-    def __next__(self):
+    def __next__(self) -> Optional[CharacterType]:
         factory = self._factory_for(self._random())
         return factory()
