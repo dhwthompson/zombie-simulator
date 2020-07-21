a50bbff07ce09d2251ca296f9979ae33f4e3e9b7
David Thompson <mail@fatbusinessman.com>
2020-07-21 14:56:21 -0700

    Add type hints to the tracing module
    Context manager type hints are a bit brain-melting, but I re-learned
    about the `@contextmanager` decorator, so that's good.

--- tracing.py
+++ tracing.py
@@ -1,9 +1,33 @@
+import attr
+from contextlib import contextmanager
 from datetime import datetime, timedelta
 import json
+from types import TracebackType
+from typing import ContextManager, Generator, Dict, List, Optional, Mapping, Type
 import uuid
 
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extensions import Protocol  # type: ignore
 
-def file_tracing(outfile):
+Context = Optional[Mapping[str, object]]
+
+
+class Tracer(Protocol):
+    def open_span(self, span_name: str, context: Context) -> None:
+        ...
+
+    def close_span(self) -> None:
+        ...
+
+
+class Writable(Protocol):
+    def write(self, out: str) -> int:
+        ...
+
+
+def file_tracing(outfile: Writable) -> ContextManager[Tracer]:
     global _tracer
 
     _tracer = FileTracer(outfile)
@@ -11,65 +35,90 @@ def file_tracing(outfile):
 
 
 class NullTracer:
-    def open_span(self, span_name, context=None):
+    def open_span(self, span_name: str, context: Context = None) -> None:
         pass
 
-    def close_span(self):
+    def close_span(self) -> None:
         pass
+
+
+@attr.s(auto_attribs=True)
+class Span:
+    id: uuid.UUID
+    name: str
+    start_time: datetime
+    context: Dict[str, object]
+
+    parent_id: Optional[uuid.UUID] = None
+    end_time: Optional[datetime] = None
+
+    def to_dict(self) -> Dict[str, object]:
+        if not self.end_time:
+            raise ValueError("Cannot serialise spans without an end time")
+        duration = self.end_time - self.start_time
+        span_dict = {
+            "id": self.id,
+            "name": self.name,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+            "duration_us": duration // timedelta(microseconds=1),
+        }
+        span_dict.update(self.context)
+        return span_dict
 
 
 class FileTracer:
-    def __init__(self, outfile):
+    def __init__(self, outfile: Writable):
         self._outfile = outfile
-        self._span_stack = []
+        self._span_stack: List[Span] = []
 
-    def __enter__(self):
+    def __enter__(self) -> Tracer:
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(
+        self,
+        type: Optional[Type[BaseException]],
+        value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
         pass
 
-    def open_span(self, span_name, context=None):
-        span = {"id": uuid.uuid4(), "name": span_name, "start_time": datetime.utcnow()}
+    def open_span(self, span_name: str, context: Context = None) -> None:
+        parent_id = self._span_stack[-1].id if self._span_stack else None
+        span = Span(
+            id=uuid.uuid4(),
+            name=span_name,
+            start_time=datetime.utcnow(),
+            context={},
+            parent_id=parent_id,
+        )
         if context:
-            span.update(context)
-        if self._span_stack:
-            span["parent_id"] = self._span_stack[-1]["id"]
+            span.context.update(context)
 
         self._span_stack.append(span)
 
-    def close_span(self):
+    def close_span(self) -> None:
         span = self._span_stack.pop()
-        span["end_time"] = datetime.utcnow()
-        microsecond = timedelta(microseconds=1)
-        span["duration_us"] = (span["end_time"] - span["start_time"]) // microsecond
-        span_str = json.dumps(span, default=str)  # Make sure the whole thing serialises
+        span.end_time = datetime.utcnow()
+        span_str = json.dumps(span.to_dict(), default=str)
         self._outfile.write(span_str + "\n")
 
 
-def open_span(span_name, context=None):
+def open_span(span_name: str, context: Context = None) -> None:
     _tracer.open_span(span_name, context)
 
 
-def close_span():
+def close_span() -> None:
     _tracer.close_span()
 
 
-def span(span_name, context=None):
-    return SpanContextManager(_tracer, span_name, context)
+@contextmanager
+def span(span_name: str, context: Context = None) -> Generator[None, None, None]:
+    open_span(span_name, context)
+    try:
+        yield
+    finally:
+        close_span()
 
 
-class SpanContextManager:
-    def __init__(self, tracer, span_name, context):
-        self._tracer = tracer
-        self._span_name = span_name
-        self._context = context
-
-    def __enter__(self):
-        self._tracer.open_span(self._span_name, self._context)
-
-    def __exit__(self, type, value, traceback):
-        self._tracer.close_span()
-
-
-_tracer = NullTracer()
+_tracer = NullTracer()  # type: Tracer
