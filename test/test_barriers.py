922b4d16b191a8ca11b9cf0977e3fcefa151ab5f
David Thompson <mail@fatbusinessman.com>
2020-09-10 16:40:15 -0700

    Expand and simplify barrier tests
    This code now covers the `positions` property, and has a single `areas`
    strategy that takes enough arguments to generate the types of areas we
    want to test.
    
    These tests are pretty slow for now, but that's manageable by limiting
    the number of test cases.

--- test/test_barriers.py
+++ test/test_barriers.py
@@ -1,28 +1,38 @@
-from hypothesis import assume, given
+from typing import FrozenSet, Iterable
+
+from hypothesis import assume, given, settings
 from hypothesis import strategies as st
 
 from barriers import Barriers
 from space import Area, Point
 
-points = st.builds(
-    Point,
-    st.integers(min_value=-50, max_value=50),
-    st.integers(min_value=-50, max_value=50),
-)
-
-areas = st.builds(Area, points, points)
-
 
 @st.composite
-def non_empty_areas(draw):
-    lower = draw(points)
+def areas(
+    draw, max_modulus=50, min_width=0, max_width=None, min_height=0, max_height=None
+):
+    lower = draw(
+        st.builds(
+            Point,
+            st.integers(min_value=-max_modulus, max_value=max_modulus - min_width),
+            st.integers(min_value=-max_modulus, max_value=max_modulus - min_height),
+        )
+    )
+
+    max_x = max_y = max_modulus
+    if max_width is not None:
+        max_x = min(max_x, lower.x + max_width)
+    if max_height is not None:
+        max_y = min(max_y, lower.y + max_height)
+
     upper = draw(
         st.builds(
             Point,
-            x=st.integers(min_value=lower.x + 1, max_value=51),
-            y=st.integers(min_value=lower.y + 1, max_value=51),
+            st.integers(min_value=lower.x + min_width, max_value=max_x),
+            st.integers(min_value=lower.y + min_height, max_value=max_y),
         )
     )
+
     return Area(lower, upper)
 
 
@@ -37,22 +47,40 @@ def area_and_point(area):
     )
 
 
+def all_points_in(areas: Iterable[Area]) -> FrozenSet[Point]:
+    points: FrozenSet[Point] = frozenset()
+    for area in areas:
+        points = points.union(area)
+    return points
+
+
 class TestBarriers:
-    @given(barrier_areas=st.sets(areas), area=areas)
+    @given(barrier_areas=st.sets(areas()), area=areas())
+    @settings(max_examples=25)
     def test_occupied_points(self, barrier_areas, area):
         barriers = Barriers.for_areas(barrier_areas)
         points = barriers.occupied_points_in(area)
+        all_area_points = all_points_in(barrier_areas)
         for point in points:
-            assert any(point in area for area in barrier_areas)
+            assert point in all_area_points
             assert barriers.occupied(point)
 
     @given(
-        barrier_areas=st.sets(areas),
-        area_and_point=non_empty_areas().flatmap(area_and_point),
+        barrier_areas=st.sets(areas()),
+        area_and_point=areas(min_width=1, min_height=1).flatmap(area_and_point),
     )
+    @settings(max_examples=25)
     def test_unoccupied_point(self, barrier_areas, area_and_point):
         area, point = area_and_point
         assume(not any(point in b for b in barrier_areas))
         barriers = Barriers.for_areas(barrier_areas)
         assert point not in barriers.occupied_points_in(area)
         assert not barriers.occupied(point)
+
+    @given(barrier_areas=st.sets(st.one_of([areas(max_width=1), areas(max_height=1)])))
+    @settings(max_examples=25)
+    def test_positions(self, barrier_areas):
+        barriers = Barriers.for_areas(barrier_areas)
+        positions = {point for (point, _) in barriers.positions}
+
+        assert positions == all_points_in(barrier_areas)
